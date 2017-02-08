590493527f7abfa52581ef1cd69cb1b731f55c48
lenovo <gannu124>
2017-02-08 18:17:30 +0530

    correction in dc table

--- munsinew.js
+++ munsinew.js
@@ -8,11 +8,11 @@ console.log(db);
 
 var table_area = "CREATE TABLE IF NOT EXISTS AREA(" +
         " ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-        " AREA_ID text, "+
-        " AREA_NAME text, "+
+        " AREA_ID TEXT, "+
+        " AREA_NAME TEXT, "+
 
-        " CREATED_TS text, " +
-        " UPDATED_TS text, " +
+        " CREATED_TS TEXT, " +
+        " UPDATED_TS TEXT, " +
         " CREATED_BY INTEGER," +
         " UPDATED_BY INTEGER)";
        db.run(table_area);
@@ -21,13 +21,13 @@ var table_area = "CREATE TABLE IF NOT EXISTS AREA(" +
          " ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
          " CP_ID INTEGER, " +
          " COMBI_PROD_ID INTEGER, "+
-         " SR text, " +                //TODO
+         " SR TEXT, " +                //TODO
          " PROD_ID INTEGER, "+
-         " PROD_DES text, "+            //TODO
+         " PROD_DES TEXT, "+            //TODO
          " QUANTITY INTEGER, "+
 
-         " CREATED_TS text, " +
-         " UPDATED_TS text, " +
+         " CREATED_TS TEXT, " +
+         " UPDATED_TS TEXT, " +
          " CREATED_BY INTEGER," +
          " UPDATED_BY INTEGER)";
       db.run(table_combidetails);
@@ -36,13 +36,13 @@ var table_area = "CREATE TABLE IF NOT EXISTS AREA(" +
      " ID INTEGER PRIMARY KEY AUTOINCREMENT," +
      " C_DATE date," +
      " CUST_ID INTEGER," +
-     " SRC_ENT text," +                    //TODO
+     " SRC_ENT TEXT," +                    //TODO
      " OP_QTY INTEGER," +
      " SUP_QTY INTEGER," +
      " RET_QTY INTEGER," +
 
-     " CREATED_TS text, " +
-     " UPDATED_TS text, " +
+     " CREATED_TS TEXT, " +
+     " UPDATED_TS TEXT, " +
      " CREATED_BY INTEGER," +
      " UPDATED_BY INTEGER)";
       db.run(table_creategl);
@@ -54,22 +54,37 @@ var table_area = "CREATE TABLE IF NOT EXISTS AREA(" +
         " CUST_CODE INTEGER, " +
         " O_NO INTEGER, " +
         " O_DATE, " +
-        " AREA text, " +
-        " S_MAN text, " +
-        " VAN text, " +
-        " GROSS_TOT real, " +
-        " SCHEME1_TOT real, " +
-        " SCHEME2_Tot real, " +
-        " DMG_TOT real, " +
-        " DISC real, " +
-        " AD real, " +
-        " LS real, " +
+        " AREA TEXT, " +
+        " S_MAN TEXT, " +
+        " VAN TEXT, " +
+        " GROSS_TOT REAL, " +
+        " SCHEME1_TOT REAL, " +
+        " SCHEME2_Tot REAL, " +
+        " DMG_TOT REAL, " +
+        " DISC REAL, " +
+        " AD REAL, " +
+        " LS REAL," +
+         "R_Off INTEGER," +
+         "FINAL_AMT REAL," +
+         "REMARKS TEXT," +
+         "COLLECTION_TYPE TEXT," +
+         "COLLECTION_DATE DATE," +
+         "COLLECTION_AMT1 REAL," +
+         "COLLECTION_AMT2 REAL," +
+         "DEP_IN TEXT," +
+         "CHQ_NO INTEGER," +
+         "CHQ_DATE DATE," +
+         "DRAWEE_BANK TEXT," +
+         "BRANCH TEXT," +
+         "CLEARING_DATE DATE," +
 
-        " CREATED_TS text, " +
-        " UPDATED_TS text, " +
+        " CREATED_TS TEXT, " +
+        " UPDATED_TS TEXT, " +
         " CREATED_BY INTEGER," +
         " UPDATED_BY INTEGER)";
      db.run(table_dc);
+
+
 
 
 
